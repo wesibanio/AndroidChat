4db98566bf8145c1d7cc7ef0f85fa13afa5d306a
Nisan Chiprut <Nisan Chiprut>
2017-03-18 17:25:16 +0200

    basic chat functionality

--- app/src/main/java/com/example/nisan/toychat/MainActivity.java
+++ app/src/main/java/com/example/nisan/toychat/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.nisan.toychat;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView outputText;
+    private EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        outputText = (TextView) findViewById(R.id.outputText);
+        inputText = (EditText) findViewById(R.id.inputText);
+        Button sendButton = (Button) findViewById(R.id.sendButton);
+
+        if (savedInstanceState != null) {
+            this.outputText.setText(savedInstanceState.getString(
+                    String.valueOf(R.id.outputText)
+            ));
+        }
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMessage();
+            }
+        });
+
+        inputText.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId != EditorInfo.IME_ACTION_SEND) {
+                    return false;
+                }
+                addMessage();
+                return true;
+            }
+        });
+
+    }
+
+    private void addMessage() {
+
+        if (getString(R.string.initMsg).equals(this.outputText.getText().toString())) {
+            this.outputText.setText("");
+        }
+        this.outputText.append(this.inputText.getText().toString() + "\n");
+
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+
+        this.inputText.setText("");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(String.valueOf(R.id.outputText), this.outputText.getText().toString());
+
     }
 }
